79425150f5720b2096c45ec7b26a6487609a141e
Muhammad Junaid <junaidsidhu123@yahoo.com>
2015-07-05 22:27:59 +0500

    listing view added

--- EstateApp 7.0/EstateApp/Classes/ViewControllers/NearByListController.swift
+++ EstateApp 7.0/EstateApp/Classes/ViewControllers/NearByListController.swift
@@ -8,6 +8,32 @@
 
 import UIKit
 
-class NearByListController: BaseViewController {
+class NearByListController: BaseViewController , UITableViewDataSource, UITableViewDelegate {
    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnMapViewPressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell;
+        
+        return cell;
+    }
 }
